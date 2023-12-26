5de8a3614984cfcb21dc86b5778131e01f37e6be
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2023-12-26 18:42:52 +0200

    front end changes

--- my-app/src/components/full_video_component.js
+++ my-app/src/components/full_video_component.js
@@ -8,6 +8,9 @@ import Scene from "./scene";
 const FullVideoComponent = (props) => {
   const { video } = useParams();
   const [currentvideo, setCurrentvideo] = useState("");
+  const [avatar, setAvatar] = useState(null);
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+
   const [title, setTitle] = useState("");
 
   useEffect(() => {
@@ -15,6 +18,9 @@ const FullVideoComponent = (props) => {
       const videos = await request("video/" + video + "/");
       setCurrentvideo(videos);
       setTitle(videos.title);
+      const avatat_options = await request("avatars/");
+      setAvatar(avatat_options);
+      setSelectedAvatar(videos.avatar);
     };
     fetchVideos();
   }, []);
@@ -26,33 +32,86 @@ const FullVideoComponent = (props) => {
     });
   };
 
+  const update_avatar = (event) => {
+    event.preventDefault();
+    axios.patch(API_BASE_URL + "/video/" + video + "/", {
+      avatar: selectedAvatar,
+    });
+  };
+
   return (
     <div className="FullvideoContainer">
       <center>
         {!currentvideo ? (
           <p></p>
         ) : (
-          <div className="fullvideo">
-            <form onSubmit={update_title} className="fullvidform">
-              <input
-                className="titletext"
-                value={title}
-                name="title"
-                onChange={(e) => setTitle(e.target.value)}
-              />
+          <>
+            <div className="fullvideo vidcontform">
+              <form onSubmit={update_title} className="fullvidform">
+                <span className="title-label">Title :</span>
+                <input
+                  className="titletext"
+                  value={title}
+                  name="title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
 
-              <button className="btn-sub" type="submit">
-                Update
-              </button>
-              <button className="btn-sub">Regenerate</button>
-              <button className="btn-sub">Render</button>
-            </form>
-            <div className="scene_container">
-              {currentvideo.scenes.map((scene) => {
-                return <Scene scene={scene} />;
-              })}
+                <button className="btn-sub" type="submit">
+                  Update
+                </button>
+              </form>
+
+              <form
+                onSubmit={update_avatar}
+                className="fullvidform avatar-select-form"
+              >
+                <span className="title-label">Avatar :</span>
+                {!avatar ? (
+                  <p>Loading...</p>
+                ) : (
+                  <select
+                    id="avatar"
+                    onChange={(e) => setSelectedAvatar(e.target.value)}
+                    name="avatar_selection"
+                    value={selectedAvatar}
+                  >
+                    <option key="No avatar" value="no_avatar">
+                      No Avatar
+                    </option>
+                    {avatar.map((avatar) => (
+                      <option key={avatar.name} value={parseInt(avatar.id)}>
+                        {avatar.name}
+                      </option>
+                    ))}
+                  </select>
+                )}
+
+                <button className="btn-sub" type="submit">
+                  Update
+                </button>
+              </form>
+              </div>
+              <div className="fullvideo">
+              <div className="scene_container">
+                {currentvideo.scenes.map((scene) => {
+                  return <Scene scene={scene} />;
+                })}
+              </div>
+
+              <div className="full-video-butts">
+                <button className="btn-sub">Regenerate</button>
+                <button
+                  className="btn-sub"
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    axios.post(API_BASE_URL + "/render/?video_id=" + video);
+                  }}
+                >
+                  Render
+                </button>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </center>
     </div>
